23ff844eab6e1abd432bfd3867d7bb5cb7cbd9e0
westmakaha <westmakaha@makawave.com>
2015-09-12 19:07:19 -0400

    add crosswalk and compile for api-19 closes #3

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -9,7 +9,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-crosswalk-webview": "1.3.1"
 }
 // BOTTOM OF METADATA
 });
