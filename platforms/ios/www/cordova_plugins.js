e65d095bd5fa8e1ee9c9ad805ee028ec6d6758eb
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-11-05 15:24:24 -0200

    Improve the initial view layout

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -3,8 +3,8 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-crosswalk-webview": "1.3.1",
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-crosswalk-webview": "1.3.1"
 }
 // BOTTOM OF METADATA
 });
