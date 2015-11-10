06305c1d6b57e53770f9b6ce190d3b0750227527
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-11-10 20:04:22 -0200

    Add webhook to compile all coffee files

---
+++ hooks/beforePrepare.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var execSync = require('child_process').execSync;
+var fs = require('fs');
+
+var coffee_path = 'www/coffee/'
+
+if( !fs.existsSync(coffee_path)) {
+    fs.mkdirSync(coffee_path)
+}
+
+execSync("coffee --compile --output www/js_compiled/ " + coffee_path)
