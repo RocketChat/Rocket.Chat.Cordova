dd8e4a73a541ee6fc43b9125b802719d1d37ddfa
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-11-28 10:57:30 -0200

    Configure bugsnag key via a ignored file

--- hooks/beforePrepare.js
+++ hooks/beforePrepare.js
@@ -13,5 +13,9 @@ execSync("rm -rf www/shared/js_compiled/*")
 execSync("coffee --compile --output www/shared/js_compiled/ " + coffee_path)
 
 if (!fs.existsSync('www/js/android_sender_id.js')) {
-	throw new Error('\n#\n# The file "www/js/android_sender_id.js" does not exists!!!\n#\n# Please create the file with content:\n#\n#    window.ANDROID_SENDER_ID = "YOUR ANDROID ID"; \n#\n#');
+	throw new Error('\n#\n# The file "www/js/android_sender_id.js" does not exists!!!\n#\n# Please create the file with content:\n#\n#    window.ANDROID_SENDER_ID = "YOUR-ANDROID-ID-HERE"; \n#\n#');
+}
+
+if (!fs.existsSync('www/js/bugsnag_apikey.js')) {
+	throw new Error('\n#\n# The file "www/js/bugsnag_apikey.js" does not exists!!!\n#\n# Please create the file with content:\n#\n#    Bugsnag.apiKey = "YOUR-API-KEY-HERE"; \n#\n#');
 }
