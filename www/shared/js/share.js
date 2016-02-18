10d6ad85eacdbacd7a946277f21b7b2064dd7e94
Travis Odom <travis.a.odom@gmail.com>
2016-02-17 21:23:30 -0600

    Use file system to store share intent data while switching servers

--- www/shared/js/share.js
+++ www/shared/js/share.js
@@ -7,46 +7,113 @@
 Meteor.subscribe('subscription', {
     onReady: function()
     {
-        var roomPrefixes = {
-            d: '@',
-            p: '&',
-            c: '#'
-        };
+        readFile(cordova.file.dataDirectory, 'pendingShare.json', function(err, shareInfo)
+        {
+            if(err)
+            {
+                return;
+            }
+
+            removeFile(cordova.file.dataDirectory, 'pendingShare.json', function() { });
+            selectRoom(JSON.parse(shareInfo));
+        });
 
         cordova.SharingReceptor.listen(function(data)
         {
-            var rooms = _.sortBy(ChatSubscription.find({open: true}).fetch(), function(room)
+            var serverOptions = '';
+            if(Servers.getServers().length > 1)
             {
-                return [room.t, room.name].join('-');
-            });
-
-            var innerString = _.reduce(rooms, function(accString, room)
-            {
-                return accString + '<option value="' + room.name + '">' + roomPrefixes[room.t] + ' ' + room.name + '</option>';
-            }, '');
-
-            var tempString = '<select id="room-select" name="room-list">' + innerString + '</select>';
-
-            swal({
-                title: 'Select room to share',
-                text: tempString,
-                html: true,
-                animation: true,
-                showCancelButton: true
-            }, function()
-            {
-                var roomName = $('#room-select')[0].value;
-                var roomModel = ChatSubscription.findOne({name: roomName});
-
-                if(roomModel)
+                serverOptions = _.reduce(Servers.getServers(), function(accString, server)
                 {
-                    doShare(roomModel, data);
-                }
-            });
+                    return accString + '<option value="' + server.url + '">' + server.name + '</option>';
+                }, '');
+
+                var serverString = '<select id="server-select" name="server-list">' + serverOptions + '</select>';
+
+                swal({
+                        title: 'Select server',
+                        text: serverString,
+                        html: true,
+                        animation: true,
+                        showCancelButton: true
+                    },
+                    function()
+                    {
+                        var currentServerUrl = Servers.getActiveServer().url;
+                        var serverUrl = $('#server-select')[0].value;
+
+                        if(serverUrl != currentServerUrl)
+                        {
+                            writeFile(cordova.file.dataDirectory, 'pendingShare.json', JSON.stringify(data), function(err, data)
+                            {
+                                if(err)
+                                {
+                                    console.log('Error saving share file', err);
+                                    return;
+                                }
+                                localStorage.setItem('pendingShare', JSON.stringify(data));
+
+                                $(document.body).addClass('loading');
+                                $('.loading-text').text(cordovai18n("Loading_s", serverUrl));
+                                setTimeout(function()
+                                {
+                                    Servers.setActiveServer(serverUrl);
+                                    Servers.startServer(serverUrl, function() { });
+                                }, 200);
+                            });
+                        }
+                        else
+                        {
+                            selectRoom(data);
+                        }
+                    });
+            }
+            else
+            {
+                selectRoom(data);
+            }
         });
 
     }
 });
+
+function selectRoom(data)
+{
+    var roomPrefixes = {
+        d: '@',
+        p: '&',
+        c: '#'
+    };
+
+    var rooms = _.sortBy(ChatSubscription.find({open: true}).fetch(), function(room)
+    {
+        return [room.t, room.name].join('-');
+    });
+
+    var innerString = _.reduce(rooms, function(accString, room)
+    {
+        return accString + '<option value="' + room.name + '">' + roomPrefixes[room.t] + ' ' + room.name + '</option>';
+    }, '');
+
+    var tempString = '<select id="room-select" name="room-list">' + innerString + '</select>';
+
+    swal({
+        title: 'Select room to share',
+        text: tempString,
+        html: true,
+        animation: true,
+        showCancelButton: true
+    }, function()
+    {
+        var roomName = $('#room-select')[0].value;
+        var roomModel = ChatSubscription.findOne({name: roomName});
+
+        if(roomModel)
+        {
+            doShare(roomModel, data);
+        }
+    });
+}
 
 function doShare(roomModel, data)
 {
